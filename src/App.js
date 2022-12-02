0670f54da0d25e653cbc821bf3a463c0a4764bf0
Umar Ashraf <79502599+UmarAxhraf@users.noreply.github.com>
2022-12-02 11:26:45 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { useState } from "react";
+//import logo from './logo.svg';
+import './App.css';
+
+const App = () => {
+  
+  return (
+    <div className='div_css'>
+    <h1>Hello</h1>
+    <input type="text" placeholder="Enter Your Name" />
+    <button>Submit</button>
+    </div>
+  );
+}
+
+export default App;
+
+{/* <img src={logo} className="App-logo" alt="logo" />
+<p>
+  Edit <code>src/App.js</code> and save to reload.
+</p> */}
---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+
+ReactDOM.render(
+  <>
+  <h1 className="heading_style">💻Welcome to <span style={{fontWeight:'bold'}}>React js</span>💻</h1>
+  <App />
+  </>,
+  document.getElementById('root')
+);
